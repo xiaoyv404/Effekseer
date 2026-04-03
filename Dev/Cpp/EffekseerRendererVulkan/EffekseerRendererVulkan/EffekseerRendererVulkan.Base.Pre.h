50d9ce0f7bd9902c3c89924d91c2205c1aec3111
xiaoyv_404 <xiaoyv_404@outlook.com>
2026-04-03 09:10:01 +0800

    Vulkan supporting

--- Dev/Cpp/EffekseerRendererVulkan/EffekseerRendererVulkan/EffekseerRendererVulkan.Base.Pre.h
+++ Dev/Cpp/EffekseerRendererVulkan/EffekseerRendererVulkan/EffekseerRendererVulkan.Base.Pre.h
@@ -6,6 +6,9 @@
 
 #ifdef _WIN32
 #define VK_USE_PLATFORM_WIN32_KHR
+#elif defined(__APPLE__)
+#define VK_ENABLE_BETA_EXTENSIONS
+#define VK_USE_PLATFORM_MACOS_MVK
 #endif
 #include <vulkan/vulkan.h>
 
